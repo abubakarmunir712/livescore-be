343bda70019cdf1209fd3865e0c5ff92370c6485
Abubakar <abubakarmunir712@gmail.com>
2025-09-20 11:39:34 +0500

    feat: add compression to reduce json payload size

--- src/configs/server.config.ts
+++ src/configs/server.config.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
-
+import compression from "compression"
 export const app = express()
+app.use(compression())
 app.use(express.json())
 app.use(cors())
